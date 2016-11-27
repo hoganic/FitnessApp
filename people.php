41794862994dbbbbd739c6203cf2d6f3dccab06d
hoganic <hoganic@bu.edu>
2016-11-27 12:31:09 -0500

    Added NULL to search

--- people.php
+++ people.php
@@ -118,18 +118,22 @@
 	}*/
 	
 	 function get_data($query) {
-		 //$query = 'chicken';
-		 $api_url = 'https://api.nutritionix.com/v1_1/search/';
-		 $parameters = '?results=0%3A1&cal_min=0&cal_max=50000&fields=nf_total_carbohydrate%2Cnf_protein%2Cnf_total_fat%2Cnf_serving_size_qty%2Cnf_serving_size_unit%2Cnf_serving_weight_grams%2Citem_name%2Cbrand_name&appId=550ff872&appKey=c6944198d0b40c218890bc459c700fdc';
-		 $request_url = $api_url.$query.$parameters;
-		 $ch = curl_init();
-		 $timeout = 5;
-		 curl_setopt($ch, CURLOPT_URL, $request_url);
-		 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-		 $data = curl_exec($ch);
-		 curl_close($ch);
-		 return $data;
+		 if ($query == NULL){
+			 return NULL;
+		 } else {
+		 	 //$query = 'chicken';
+		 	 $api_url = 'https://api.nutritionix.com/v1_1/search/';
+			 $parameters = '?results=0%3A1&cal_min=0&cal_max=50000&fields=nf_total_carbohydrate%2Cnf_protein%2Cnf_total_fat%2Cnf_serving_size_qty%2Cnf_serving_size_unit%2Cnf_serving_weight_grams%2Citem_name%2Cbrand_name&appId=550ff872&appKey=c6944198d0b40c218890bc459c700fdc';
+			 $request_url = $api_url.$query.$parameters;
+			 $ch = curl_init();
+			 $timeout = 5;
+			 curl_setopt($ch, CURLOPT_URL, $request_url);
+			 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+			 $data = curl_exec($ch);
+			 curl_close($ch);
+			 return $data;
+		 }
 	 }
 ?> 
 	

f40a0139d473df7b061d1110504aee5591553ce7
hoganic <hoganic@bu.edu>
2016-11-26 22:39:08 -0500

    Update people.php

--- people.php
+++ people.php
@@ -110,6 +110,34 @@
     </tr>
 </table>
 </div>
+	
+<div class="right-message">
+<?php
+$returned_content = get_data("https://api.nutritionix.com/v1_1/search/pop%20tart?results=0%3A1&cal_min=0&cal_max=50000&fields=nf_total_carbohydrate%2Cnf_protein%2Cnf_total_fat%2Cnf_serving_size_qty%2Cnf_serving_size_unit%2Cnf_serving_weight_grams%2Citem_name%2Cbrand_name&appId=550ff872&appKey=c6944198d0b40c218890bc459c700fdc");
+$array = json_decode($returned_content, TRUE);
+foreach($array["hits"] as $hits){
+	echo "Item name: ".$hits["fields"]["item_name"]."<br>";
+	echo "Brand name: ".$hits["fields"]["brand_name"]."<br>";
+	echo "Serving size (quantity): ".$hits["fields"]["nf_serving_size_qty"]."<br>";
+	echo "Serving size (unit): ".$hits["fields"]["nf_serving_size_unit"]."<br>";
+	echo "Serving size (grams): ".$hits["fields"]["nf_serving_weight_grams"]."<br>";
+	echo "Total carbs: ".$hits["fields"]["nf_total_carbohydrate"]."<br>";
+	echo "Total protein: ".$hits["fields"]["nf_protein"]."<br>";
+	echo "Total fat: ".$hits["fields"]["nf_total_fat"]."<br><br>";
+}
+	function get_data($url) {
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+?>
+	</div>
+	
 <div id="mydata">
 <b>Your Current Plan</b>
 <table id="myTableData"  border="1" cellpadding="2">
@@ -141,32 +169,7 @@
 
 	Currently set for only 1 result for simplicity
   -->
-<div class="right-message">
-<?php
-$returned_content = get_data("https://api.nutritionix.com/v1_1/search/pop%20tart?results=0%3A1&cal_min=0&cal_max=50000&fields=nf_total_carbohydrate%2Cnf_protein%2Cnf_total_fat%2Cnf_serving_size_qty%2Cnf_serving_size_unit%2Cnf_serving_weight_grams%2Citem_name%2Cbrand_name&appId=550ff872&appKey=c6944198d0b40c218890bc459c700fdc");
-$array = json_decode($returned_content, TRUE);
-foreach($array["hits"] as $hits){
-	echo "Item name: ".$hits["fields"]["item_name"]."<br>";
-	echo "Brand name: ".$hits["fields"]["brand_name"]."<br>";
-	echo "Serving size (quantity): ".$hits["fields"]["nf_serving_size_qty"]."<br>";
-	echo "Serving size (unit): ".$hits["fields"]["nf_serving_size_unit"]."<br>";
-	echo "Serving size (grams): ".$hits["fields"]["nf_serving_weight_grams"]."<br>";
-	echo "Total carbs: ".$hits["fields"]["nf_total_carbohydrate"]."<br>";
-	echo "Total protein: ".$hits["fields"]["nf_protein"]."<br>";
-	echo "Total fat: ".$hits["fields"]["nf_total_fat"]."<br><br>";
-}
-	function get_data($url) {
-		$ch = curl_init();
-		$timeout = 5;
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-		$data = curl_exec($ch);
-		curl_close($ch);
-		return $data;
-	}
-?>
-	</div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="../../dist/js/bootstrap.min.js"></script>

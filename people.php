33634ba8ab0f88e6b73a258ad77187ef3ed18f46
hoganic <hoganic@bu.edu>
2016-11-26 22:17:30 -0500

    updated JS link

--- people.php
+++ people.php
@@ -3,6 +3,7 @@
 <head>
   <title> Meal Planning Page</title>
   <meta charset="UTF-8">
+  <script type="text/javascript" src="app.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>

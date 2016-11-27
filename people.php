ecc97c4eba681240e863fe8d73ed7625c0939bf2
hoganic <hoganic@bu.edu>
2016-11-26 22:58:33 -0500

    Update people.php

--- people.php
+++ people.php
@@ -123,11 +123,12 @@
 </div>
 	
 <div class="flex-item">
+<title> Food Search </title>
 <?php
 $returned_content = get_data("https://api.nutritionix.com/v1_1/search/pop%20tart?results=0%3A1&cal_min=0&cal_max=50000&fields=nf_total_carbohydrate%2Cnf_protein%2Cnf_total_fat%2Cnf_serving_size_qty%2Cnf_serving_size_unit%2Cnf_serving_weight_grams%2Citem_name%2Cbrand_name&appId=550ff872&appKey=c6944198d0b40c218890bc459c700fdc");
 $array = json_decode($returned_content, TRUE);
 foreach($array["hits"] as $hits){
-	echo "Item name: ".$hits["fields"]["item_name"]."<br>";
+	echo "Item name: ".$hits["item_name"]."<br>";
 	echo "Brand name: ".$hits["fields"]["brand_name"]."<br>";
 	echo "Serving size (quantity): ".$hits["fields"]["nf_serving_size_qty"]."<br>";
 	echo "Serving size (unit): ".$hits["fields"]["nf_serving_size_unit"]."<br>";

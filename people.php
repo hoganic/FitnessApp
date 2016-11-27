3ccae292cb33acec74e022320f0e6a58e1e3e001
hoganic <hoganic@bu.edu>
2016-11-26 22:53:06 -0500

    Update people.php

--- people.php
+++ people.php
@@ -61,10 +61,19 @@
     top:5px;
     right:5px;
 }
+.container{
+    display: flex;
+}
+.fixed{
+    width: 200px;
+}
+.flex-item{
+    flex-grow: 1;
+}
 </style>
 
 <body onload="load()">
-<div id="myform">
+<<div class="fixed">
 <b><strong>MEAL PLANNING</strong></b>
 <table>
     <tr>
@@ -110,7 +119,7 @@
 </table>
 </div>
 	
-<div class="right-message">
+<div class="flex-item">
 <?php
 $returned_content = get_data("https://api.nutritionix.com/v1_1/search/pop%20tart?results=0%3A1&cal_min=0&cal_max=50000&fields=nf_total_carbohydrate%2Cnf_protein%2Cnf_total_fat%2Cnf_serving_size_qty%2Cnf_serving_size_unit%2Cnf_serving_weight_grams%2Citem_name%2Cbrand_name&appId=550ff872&appKey=c6944198d0b40c218890bc459c700fdc");
 $array = json_decode($returned_content, TRUE);

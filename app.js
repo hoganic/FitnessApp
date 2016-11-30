2fcdec07c7fc141a9c139d5ee6f501b21d247339
hoganic <hoganic@bu.edu>
2016-11-29 23:50:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function addRow() {
     row.insertCell(8).innerHTML= (fat.value*9 + carbs.value*4 + protein.value*4)*amount.value;
 }
 
-function finishTable() {
+window.onload = function finishTable() {
     var tableElemName = "myTableData";
     
     var totalCarbs = computeTableColumnTotal(tableElemName, 5);
@@ -82,69 +82,7 @@ function deleteRow(obj) {
     
 }
 
-function edit_row(no) {
- document.getElementById("edit_button"+no).style.display="none";
- document.getElementById("save_button"+no).style.display="block";
 
- var mealno = document.getElementById("mealno"+no);      
- var myFood = document.getElementById("food"+no);
- var serving_size_unit = document.getElementById("serving_size_unit"+no);
- var amount = document.getElementById("amount"+no);
- var carbs = document.getElementById("carbs"+no);
- var protein = document.getElementById("protein"+no);
- var fat = document.getElementById("fat"+no);
-    
- var mealno_data = mealno.innerHTML;      
- var myFood_data = myFood.innerHTML;
- var serving_size_unit_data = serving_size_unit.innerHTML;
- var amount_data = amount.innerHTML;
- var carbs_data = carbs.innerHTML;
- var protein_data = protein.innerHTML;
- var fat_data = fat.innerHTML;
-    
- mealno.innerHTML="<input type='text' id='mealno_text"+no+"' value='"+mealno_data+"'>";
- amount.innerHTML="<input type='text' id='amount_text"+no+"' value='"+amount_data+"'>";
-}
-
-function save_row(no) {
- var name_val=document.getElementById("name_text"+no).value;
- var country_val=document.getElementById("country_text"+no).value;
- var age_val=document.getElementById("age_text"+no).value;
-
- document.getElementById("name_row"+no).innerHTML=name_val;
- document.getElementById("country_row"+no).innerHTML=country_val;
- document.getElementById("age_row"+no).innerHTML=age_val;
-
- document.getElementById("edit_button"+no).style.display="block";
- document.getElementById("save_button"+no).style.display="none";
-}
-
-
-function addTable() {
-      
-    var myTableDiv = document.getElementById("myDynamicTable");
-      
-    var table = document.createElement('TABLE');
-    table.border='1';
-    
-    var tableBody = document.createElement('TBODY');
-    table.appendChild(tableBody);
-      
-    for (var i=0; i<3; i++){
-       var tr = document.createElement('TR');
-       tableBody.appendChild(tr);
-       
-       for (var j=0; j<4; j++){
-           var td = document.createElement('TD');
-           td.width='75';
-           td.appendChild(document.createTextNode("Cell " + i + "," + j));
-           tr.appendChild(td);
-       }
-    }
-    myTableDiv.appendChild(table);
-    
-}
- 
 function load() {
     
     console.log("Page load finished");

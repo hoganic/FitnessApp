636ef4eaad6fb7dd66cec133219bd6cf02e9e2a4
sarwood <sarwood@bu.edu>
2016-12-10 19:00:59 -0500

    fb integration

--- calculatorTemplateUI.php
+++ calculatorTemplateUI.php
@@ -269,7 +269,7 @@ calories.value=(carbs.value*4+fat.value*9+protein.value*4).toFixed(0);
       version    : 'v2.7'
     });
     FB.AppEvents.logPageView();
-
+  }
   // Load the SDK asynchronously
   (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];

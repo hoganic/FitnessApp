c79f00f28e0cb9be1fbf7fdc9f5ca2b43cb676ea
Aastha Anand <aastha24@bu.edu>
2016-12-11 20:44:49 -0500

    Update calculatorTemplateUI.php

--- calculatorTemplateUI.php
+++ calculatorTemplateUI.php
@@ -85,7 +85,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Calculate your Macros</a>
+			<a class="navbar-brand" href="#"><b>Calculate your Macros</b></a>
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-left">

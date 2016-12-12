fc32dde114f8d47ddc4cfe711a9fdc432ee1fda7
Aastha Anand <aastha24@bu.edu>
2016-12-12 00:54:29 -0500

    Update peopleUI.php

--- peopleUI.php
+++ peopleUI.php
@@ -136,10 +136,10 @@
 					&nbsp;
 				</div>
 				<form>
-					&nbsp;&nbsp; User Macros <input type="text" name="UserCarbs" id="UserCarbs" value="0">&nbsp;Carbs
-					<input type="text" name="UserProtein" id="UserProtein" value="0">&nbsp;Protein
-					<input type="text" name="UserFat" id="UserFat" value="0">&nbsp;Fat
-					<input type="text" name="UserCalories" id="UserCalories" value="0">&nbsp;Calories
+					&nbsp;User Macros <input type="text" name="UserCarbs" id="UserCarbs" value="0">Carbs
+					<input type="text" name="UserProtein" id="UserProtein" value="0">Protein
+					<input type="text" name="UserFat" id="UserFat" value="0">Fat
+					<input type="text" name="UserCalories" id="UserCalories" value="0">Calories
 				</form>
 					&nbsp;
 				<div class="header">
